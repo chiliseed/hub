f9a46cecaafe1bd7d8a7e8dc6fe67e7def11e5ed
ana <ana@altonotch.com>
2019-10-13 15:35:43 +0300

    Make user model inherit from base model

--- src/users/models.py
+++ src/users/models.py
@@ -1,15 +1,7 @@
-from hashlib import blake2b
-from uuid import uuid4
-
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+
+from utils.models import BaseModel
 
 
-class User(AbstractUser):
+class User(AbstractUser, BaseModel):
     """Chiliseed user object."""
-
-    slug = models.SlugField(max_length=20, null=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = blake2b(uuid4().bytes, digest_size=10).hexdigest()
-        super().save(*args, **kwargs)

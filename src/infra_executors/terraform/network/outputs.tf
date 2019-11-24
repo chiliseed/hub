cad2ba1cc6cb51e45578c3e685f448e896132517
ana <ana@altonotch.com>
2019-11-24 13:50:07 +0200

    Fix network outputs to be flat lists

--- src/infra_executors/terraform/network/outputs.tf
+++ src/infra_executors/terraform/network/outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = module.network.vpc_id
 }
+
+output "private_subnet_ids" {
+  value = flatten(module.network.private_subnet_ids)
+}
+
+output "public_subnet_ids" {
+  value = flatten(module.network.public_subnet_ids)
+}
+
+output "depends_id" {
+  value = module.network.depends_id
+}

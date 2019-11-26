b348aaedf07d90bddafa030b9da517951de944a5
ana <ana@altonotch.com>
2019-11-26 23:25:01 +0200

    Refactor network to work with terraform executor

--- src/infra_executors/network.py
+++ src/infra_executors/network.py
@@ -1,141 +1,51 @@
 """Creates the vpc and it's network."""
 import argparse
 import logging
-import os
+from typing import Mapping
 
 from common.crypto import get_uuid_hex
 
 from infra_executors.constants import (
     AwsCredentials,
-    EXEC_LOGS_DIR,
     GeneralConfiguration,
-    PLANS_DIR,
-    TERRAFORM_DIR,
-    TERRAFORM_PLUGIN_DIR,
 )
-from infra_executors.utils import execute_shell_command, extract_outputs
+from infra_executors.terraform_executor import (
+    ExecutorConfiguration,
+    TerraformExecutor,
+)
 
 
 logger = logging.getLogger(__name__)
-NETWORK_DIR = os.path.join(TERRAFORM_DIR, "network")
 
 
 def create_network(
     creds: AwsCredentials, params: GeneralConfiguration,
-) -> (str, None):
+) -> Mapping[str, str]:
     """Create vpc with private and subnet network, with internet access."""
-    env_vars = dict(
-        AWS_ACCESS_KEY_ID=creds.access_key,
-        AWS_SECRET_ACCESS_KEY=creds.secret_key,
-        AWS_SESSION_TOKEN=creds.session_key,
-        AWS_DEFAULT_REGION=creds.region,
-        TF_VAR_environment=params.env_name,
+    executor = TerraformExecutor(
+        creds,
+        params,
+        cmd_configs=None,
+        executor_configs=ExecutorConfiguration(
+            "network_create", "network", "network"
+        ),
     )
-    logger.info("Executing run id=%s", params.run_id)
-
-    run_log = os.path.join(
-        EXEC_LOGS_DIR, f"network_create_{params.run_id}.log"
-    )
-
-    logger.info("Initializing terraform state: %s", env_vars)
-
-    init_cmd = [
-        f"terraform init "
-        f'-backend-config="key={params.project_name}/network.tfstate" '
-        f"-no-color -reconfigure -plugin-dir {TERRAFORM_PLUGIN_DIR}"
-    ]
-    init_return_code = execute_shell_command(
-        init_cmd, env_vars, NETWORK_DIR, log_to=run_log
-    )
-    if init_return_code != 0:
-        logger.error(
-            "Failed to init terraform for backend %s", params.project_name
-        )
-        return
-
-    logger.info("Planning terraform changes. run_id=%s", params.run_id)
-    plan_file = os.path.join(PLANS_DIR, f"network_{params.run_id}.tfplan")
-    plan_cmd = [
-        f"terraform plan "
-        f"-detailed-exitcode "
-        f"-no-color "
-        f"-var-file=network.tfvars "
-        f"-out={plan_file}",
-    ]
-    plan_return_code = execute_shell_command(
-        plan_cmd, env_vars, NETWORK_DIR, log_to=run_log
-    )
-
-    if plan_return_code == 0:
-        logger.info("No changes to apply")
-        return
-
-    if plan_return_code == 1 or plan_return_code < 0:
-        logger.error("Error executing network plan")
-        return
-
-    logger.info("Got changes to apply")
-    apply_cmd = [
-        f"terraform apply -auto-approve -no-color {plan_file}",
-    ]
-    apply_return_code = execute_shell_command(
-        apply_cmd, env_vars, NETWORK_DIR, log_to=run_log
-    )
-    if apply_return_code != 0:
-        logger.error("Failed to apply a plan %s", plan_file)
-    else:
-        vpc_outputs = extract_outputs(run_log)
-        logger.info("Created new vpc id=%s", vpc_outputs["vpc_id"])
-        return vpc_outputs["vpc_id"]
+    return executor.execute_apply()
 
 
 def destroy_network(
     creds: AwsCredentials, params: GeneralConfiguration,
-) -> (str, None):
+) -> None:
     """Destroy vpc that was created with create_network from above."""
-    env_vars = dict(
-        AWS_ACCESS_KEY_ID=creds.access_key,
-        AWS_SECRET_ACCESS_KEY=creds.secret_key,
-        AWS_SESSION_TOKEN=creds.session_key,
-        AWS_DEFAULT_REGION=creds.region,
-        TF_VAR_environment=params.env_name,
+    executor = TerraformExecutor(
+        creds,
+        params,
+        cmd_configs=None,
+        executor_configs=ExecutorConfiguration(
+            "network_destroy", "network", "network"
+        ),
     )
-    logger.info("Executing run id=%s", params.run_id)
-    run_log = os.path.join(
-        EXEC_LOGS_DIR, f"network_destroy_{params.run_id}.log"
-    )
-
-    logger.info("Initializing terraform state: %s", env_vars)
-
-    init_cmd = [
-        f"terraform init "
-        f'-backend-config="key={params.project_name}/network.tfstate" '
-        f"-no-color "
-        f"-reconfigure "
-        f"-plugin-dir {TERRAFORM_PLUGIN_DIR}"
-    ]
-    init_return_code = execute_shell_command(
-        init_cmd, env_vars, NETWORK_DIR, log_to=run_log
-    )
-    if init_return_code < 0:
-        logger.error(
-            "Failed to init terraform for backend %s", params.project_name
-        )
-        return
-
-    destroy_cmd = [
-        f"terraform destroy "
-        f"-auto-approve "
-        f"-no-color "
-        f"-var-file=network.tfvars"
-    ]
-    destroy_return_code = execute_shell_command(
-        destroy_cmd, env_vars, NETWORK_DIR, log_to=run_log
-    )
-    if destroy_return_code < 0:
-        logger.error("Failed to destroy network. run_id=%s", params.run_id)
-
-    return
+    return executor.execute_destroy()
 
 
 if __name__ == "__main__":

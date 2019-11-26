13ffdbfa5ba520ba006d9ef9bee11e5d5681213f
ana <ana@altonotch.com>
2019-11-26 22:03:24 +0200

    Linter fixes

--- src/infra_executors/utils.py
+++ src/infra_executors/utils.py
@@ -1,12 +1,18 @@
+"""Common infra executors utilities."""
 import logging
 import re
-import subprocess
-
+import subprocess  # nosec
+from typing import List, Mapping, Optional
 
 logger = logging.getLogger(__name__)
 
 
-def execute_shell_command(cmd: list, env_vars: dict = None, cwd=None, log_to=None):
+def execute_shell_command(
+    cmd: List[str],
+    env_vars: Optional[Mapping[str, str]],
+    cwd: Optional[str],
+    log_to: Optional[str],
+) -> int:
     """Execute provided command within shell.
 
     Parameters
@@ -24,25 +30,32 @@ def execute_shell_command(cmd: list, env_vars: dict = None, cwd=None, log_to=Non
 
     Returns
     -------
-        process return code : int
+    process return code : int
+        0 - success
+        1 - error
+        -N - process was terminated by signal N
     """
     with subprocess.Popen(
         cmd,
-        shell=True,
+        shell=True,  # nosec
         env=env_vars,
         cwd=cwd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
     ) as process:
-        with open(log_to, 'ab') as logfile:
+        if log_to:
+            with open(log_to, "ab") as logfile:
+                for line in process.stdout:
+                    logfile.write(line)
+                    logger.info(line.decode())
+        else:
             for line in process.stdout:
-                logfile.write(line)
                 logger.info(line.decode())
         process.poll()
     return process.returncode
 
 
-def extract_outputs(log_file_path):
+def extract_outputs(log_file_path: str) -> Mapping[str, str]:
     """Extract terraform outputs as python objects.
 
     Parameters
@@ -52,22 +65,22 @@ def extract_outputs(log_file_path):
 
     Returns
     -------
-        outputs : dict
-            key values of extracted terraform outputs
+    outputs : dict
+        key values of extracted terraform outputs
     """
     output_lines = []
 
-    with open(log_file_path, 'r') as log_file:
+    with open(log_file_path, "r") as log_file:
         for line in reversed(log_file.readlines()):
-            if 'Outputs:' in line:
+            if "Outputs:" in line:
                 break
             output_lines.append(line)
 
     outputs_joined = "".join(reversed(output_lines)).strip()
     # remove all '\n   '
-    clean_output1 = re.sub("[\\n]\s+", "", outputs_joined)
+    clean_output1 = re.sub(r"[\\n]\s+", "", outputs_joined)
     # replace all ',\n]' with ']'
-    clean_output2 = re.sub(",[\\n]]", "]", clean_output1)
+    clean_output2 = re.sub(r",[\\n]]", "]", clean_output1)
 
     outputs = {}
     for clean_line in clean_output2.splitlines():

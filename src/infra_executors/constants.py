be0e269c46c22e35cf604f38fc69c189afffab8c
ana <ana@altonotch.com>
2019-11-26 12:58:28 +0200

    Move aws creds and general configs to constants

--- src/infra_executors/constants.py
+++ src/infra_executors/constants.py
@@ -1,4 +1,5 @@
 import os
+from typing import NamedTuple
 
 from control_center.settings import BASE_DIR
 
@@ -7,3 +8,20 @@ TERRAFORM_DIR = os.path.join(INFRA_DIR, "terraform")
 EXEC_LOGS_DIR = os.path.join(INFRA_DIR, "exec_logs")
 TERRAFORM_PLUGIN_DIR = os.path.join("~", ".terraform.d", "plugins")
 PLANS_DIR = os.path.join(INFRA_DIR, "terraform_plans")
+
+
+class AwsCredentials(NamedTuple):
+    """AWS credentials configs."""
+
+    access_key: str
+    secret_key: str
+    session_key: str
+    region: str = "us-east-1"  # N.Virginia
+
+
+class GeneralConfiguration(NamedTuple):
+    """Common execution configs."""
+
+    project_name: str
+    env_name: str
+    run_id: str

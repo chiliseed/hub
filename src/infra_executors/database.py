7c76e6fa0fdec80b86bb7db78190accbe7552824
ana <ana@altonotch.com>
2019-11-26 23:25:12 +0200

    Add postgres builder

---
+++ src/infra_executors/database.py
@@ -0,0 +1,90 @@
+"""Creates the database inside a vpc."""
+import argparse
+import logging
+from typing import NamedTuple, Mapping
+
+from infra_executors.constants import AwsCredentials, GeneralConfiguration
+from infra_executors.terraform_executor import TerraformExecutor, ExecutorConfiguration
+
+logger = logging.getLogger(__name__)
+
+
+class DBConfigs(NamedTuple):
+    identifier: str
+    name: str
+    username: str
+    password: str
+
+    instance_type: str = "db.t2.large"
+    allocated_storage: int = 100
+
+
+def create_postgresql(
+    creds: AwsCredentials, params: GeneralConfiguration, db_conf: DBConfigs
+) -> Mapping[str, str]:
+    """Create a database."""
+    logger.info("Executing run id=%s", params.run_id)
+    executor = TerraformExecutor(creds, params, db_conf, ExecutorConfiguration("postgres_create", "postgres", "db"))
+    return executor.execute_apply()
+
+
+def destroy_postgresql(
+    creds: AwsCredentials, params: GeneralConfiguration, db_conf: DBConfigs
+) -> None:
+    """Destroy database."""
+    logger.info("Executing destroy postgres. run_id=%s", params.run_id)
+    executor = TerraformExecutor(creds, params, db_conf, ExecutorConfiguration("postgres_create", "postgres", "db"))
+    executor.execute_destroy()
+    logger.info("DB destroyed")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create/destroy db in the provides vpc."
+    )
+    parser.add_argument(
+        "cmd",
+        type=str,
+        default="create",
+        help="Sub command. One of: create/destroy",
+    )
+    parser.add_argument(
+        "project_name",
+        type=str,
+        help="The name of your project. Example: chiliseed",
+    )
+    parser.add_argument(
+        "environment",
+        type=str,
+        default="develop",
+        help="The name of your environment. Example: develop",
+    )
+    parser.add_argument("--aws-access-key", type=str, dest="aws_access_key")
+    parser.add_argument("--aws-secret-key", type=str, dest="aws_secret_key")
+    parser.add_argument(
+        "--aws-region", type=str, default="us-east-2", dest="aws_region"
+    )
+    parser.add_argument(
+        "--run-id", type=str, default=1, dest="run_id"
+    )
+
+    args = parser.parse_args()
+
+    aws_creds = AwsCredentials(
+        args.aws_access_key, args.aws_secret_key, "", args.aws_region
+    )
+    common = GeneralConfiguration(
+        args.project_name, args.environment, args.run_id
+    )
+    cmd_configs = DBConfigs(
+        identifier="chiliseed_executor_test",
+        name="control_center",
+        username="chiliseed",
+        password="1ZrXivL86bSr",
+        allocated_storage=5,
+    )
+
+    if args.cmd == "create":
+        create_postgresql(aws_creds, common, cmd_configs)
+    if args.cmd == "destroy":
+        destroy_postgresql(aws_creds, common, cmd_configs)

269e825b41756ef3a63245524d88bc87170a2ffc
ana <ana@altonotch.com>
2019-11-27 11:56:46 +0200

    Lint fixes

--- src/common/crypto.py
+++ src/common/crypto.py
@@ -3,6 +3,6 @@ from hashlib import blake2b
 from uuid import uuid4
 
 
-def get_uuid_hex(digest_size=10):
+def get_uuid_hex(digest_size: int = 10) -> str:
     """Generate hex of uuid4 with the defined size."""
     return blake2b(uuid4().bytes, digest_size=digest_size).hexdigest()

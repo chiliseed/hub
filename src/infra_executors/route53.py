c6fe3dbcc2aeb3de33b38cfe7c7b0f17c793a5b4
ana <ana@altonotch.com>
2020-01-19 19:36:09 +0200

    Lint fixes in route53 executor

--- src/infra_executors/route53.py
+++ src/infra_executors/route53.py
@@ -1,25 +1,37 @@
+"""Manages Route53 resource."""
 import argparse
-from typing import NamedTuple, List
+from typing import Any, List, NamedTuple
 
-from infra_executors.constants import GeneralConfiguration, AwsCredentials
+from infra_executors.constants import AwsCredentials, GeneralConfiguration
 from infra_executors.constructors import build_state_key
 from infra_executors.logger import get_logger
-from infra_executors.terraform_executor import TerraformExecutor, ExecutorConfiguration
+from infra_executors.terraform_executor import (
+    ExecutorConfiguration,
+    TerraformExecutor,
+)
 
 logger = get_logger("r53-infra-executor")
 
 
 class CnameSubDomain(NamedTuple):
+    """Subdomain configuration."""
+
     subdomain: str
     route_to: str
 
 
 class Route53Configuration(NamedTuple):
+    """Route53 configs."""
+
     domain: str
     cname_subdomains: List[CnameSubDomain]
 
 
-def create_route53(creds: AwsCredentials, params: GeneralConfiguration, run_config: Route53Configuration):
+def create_route53(
+    creds: AwsCredentials,
+    params: GeneralConfiguration,
+    run_config: Route53Configuration,
+) -> Any:
     """Create and apply changes in route 53."""
     logger.info("Executing run_id=%s", params.run_id)
     executor = TerraformExecutor(
@@ -31,13 +43,17 @@ def create_route53(creds: AwsCredentials, params: GeneralConfiguration, run_conf
             action="create",
             config_dir="route53",
             state_key=build_state_key(params, "route53"),
-            variables_file_name=""
-        )
+            variables_file_name="",
+        ),
     )
     return executor.execute_apply()
 
 
-def destroy_route53(creds: AwsCredentials, params: GeneralConfiguration, run_config: Route53Configuration):
+def destroy_route53(
+    creds: AwsCredentials,
+    params: GeneralConfiguration,
+    run_config: Route53Configuration,
+) -> Any:
     """Create and apply changes in route 53."""
     logger.info("Executing run_id=%s", params.run_id)
     executor = TerraformExecutor(
@@ -49,8 +65,8 @@ def destroy_route53(creds: AwsCredentials, params: GeneralConfiguration, run_con
             action="destroy",
             config_dir="route53",
             state_key=build_state_key(params, "route53"),
-            variables_file_name=""
-        )
+            variables_file_name="",
+        ),
     )
     return executor.execute_destroy()
 
@@ -81,9 +97,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--aws-region", type=str, default="us-east-2", dest="aws_region"
     )
-    parser.add_argument(
-        "--run-id", type=str, default=1, dest="run_id"
-    )
+    parser.add_argument("--run-id", type=str, default=1, dest="run_id")
 
     args = parser.parse_args()
 
@@ -95,7 +109,11 @@ if __name__ == "__main__":
     )
     cmd_configs = Route53Configuration(
         domain="chiliseed.com",
-        cname_subdomains=[CnameSubDomain("demo", "demo-alb-1852262830.us-east-2.elb.amazonaws.com")]
+        cname_subdomains=[
+            CnameSubDomain(
+                "demo", "demo-alb-1852262830.us-east-2.elb.amazonaws.com"
+            )
+        ],
     )
 
     if args.cmd == "create":

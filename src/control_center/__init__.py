83ce3f79369486489a1491b32305d362e52ab2a3
ana <ana@altonotch.com>
2020-02-14 00:26:06 +0200

    Update scheduler
    - don’t start if in tests
    - use atexit instead of signals

--- src/control_center/__init__.py
+++ src/control_center/__init__.py
@@ -1,18 +1,1 @@
 """Main configuration module for Chiliseed API."""
-import signal
-import sys
-
-
-def handle_shutdown(signum, frame):
-    from .scheduler import scheduler
-
-    print("Signal handler called with signal ", signum)
-    print("waiting for scheduler jobs to finish...")
-    scheduler.shutdown()
-    print("stopped".upper())
-    sys.exit(0)
-
-
-print("Registering to kill events")
-signal.signal(signal.SIGINT, handle_shutdown)
-signal.signal(signal.SIGTERM, handle_shutdown)
--- src/control_center/scheduler.py
+++ src/control_center/scheduler.py
@@ -1,10 +1,16 @@
+import os
+import atexit
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore, register_events
 
-
 scheduler = BackgroundScheduler()
-scheduler.add_jobstore(DjangoJobStore(), "default")
-register_events(scheduler)
 
-scheduler.start()
-print("Scheduler started")
+if not "PYTEST_CURRENT_TEST" in os.environ:
+    atexit.register(lambda: scheduler.shutdown(wait=False))
+
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+    register_events(scheduler)
+
+    scheduler.start()
+    print("Scheduler started")

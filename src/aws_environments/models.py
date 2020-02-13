e4a88250a2b4553aa6dbc09781e33b2f38835d7f
ana <ana@altonotch.com>
2020-02-14 00:29:47 +0200

    Remove debug message

--- src/aws_environments/models.py
+++ src/aws_environments/models.py
@@ -33,7 +33,6 @@ class OptionalSchemeURLValidator(URLValidator):
     def __call__(self, value):
         if "://" not in value:
             value = "http://" + value
-        print("validating: ", value)
         super().__call__(value)
 
 

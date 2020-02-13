9352eb1ff76835b8deac8cd5684488de4c908aa7
ana <ana@altonotch.com>
2020-02-13 16:57:50 +0200

    Disable infra executors logger

--- src/infra_executors/logger.py
+++ src/infra_executors/logger.py
@@ -4,7 +4,7 @@ import sys
 FORMAT = (
     "%(asctime)-27s %(name)-22s %(funcName)s %(filename)s %(levelname)s %(message)s"
 )
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
 
 
 def get_logger(name: str) -> logging.Logger:

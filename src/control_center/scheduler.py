577216860256acf4463719ec29feba4eb47d8971
ana <ana@altonotch.com>
2020-02-18 00:39:06 +0200

    Update scheduler

--- src/control_center/scheduler.py
+++ src/control_center/scheduler.py
@@ -7,10 +7,11 @@ from django_apscheduler.jobstores import DjangoJobStore, register_events
 scheduler = BackgroundScheduler()
 
 if not "PYTEST_CURRENT_TEST" in os.environ:
-    atexit.register(lambda: scheduler.shutdown(wait=False))
 
     scheduler.add_jobstore(DjangoJobStore(), "default")
     register_events(scheduler)
 
     scheduler.start()
     print("Scheduler started")
+
+    atexit.register(lambda: scheduler.shutdown(wait=False))

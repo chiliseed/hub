5106e30f16a291400dc89125a9a9b5d649c191b5
ana <ana@altonotch.com>
2020-02-18 15:39:38 +0200

    set django settings module by default to dev

--- src/manage.py
+++ src/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Execute django commands."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "control_center.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "control_center.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
